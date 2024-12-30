28a50f1bdb12367d872e1f1a201bc53e93332ded
Edwy Lugo <edwylugo@gmail.com>
2024-12-30 09:26:46 -0300

    feature: create extensions

--- CoinApp/DesignSystem/Extensions/String.swift
+++ CoinApp/DesignSystem/Extensions/String.swift
@@ -6,107 +6,70 @@
 //
 
 import Foundation
-import UIKit
 
 extension String {
     func formatDateToEUA() -> String {
-        if self.count > 0 {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd"
-            let date = dateFormatter.date(from: self)
-            dateFormatter.dateFormat = "MM/dd/yyyy"
-            return  dateFormatter.string(from: date!)
-        } else {
-            return "-"
-        }
+        guard !self.isEmpty else { return "-" }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        guard let date = dateFormatter.date(from: self) else { return "-" }
+        
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter.string(from: date)
     }
     
     func formatDateTimeToPtEUADateTime() -> String {
-        if self.count > 15 {
-            let inputFormatter = DateFormatter()
-            inputFormatter.dateFormat = "yyyy-MM-dd HH:mm"
-            inputFormatter.timeZone = TimeZone(abbreviation: "GMT")
-            var string = self.replacingOccurrences(of: "T", with: " ")
-            string = string.substring(to: 15)
-            if let date = inputFormatter.date(from: string) {
-                
-                let outputFormatter = DateFormatter()
-                outputFormatter.dateFormat = "MM/dd/yyyy HH:mm"
-                return outputFormatter.string(from: date)
-            }
-            
-        }
-        return self
+        guard self.count > 15 else { return self }
+        
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        inputFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        
+        let cleanedString = self.replacingOccurrences(of: "T", with: " ").prefix(16)
+        
+        guard let date = inputFormatter.date(from: String(cleanedString)) else { return self }
+        
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = "MM/dd/yyyy HH:mm"
+        return outputFormatter.string(from: date)
     }
     
-    // Substring
-    func substring(from: Int?, to: Int?) -> String {
-        if let start = from {
-            guard start < self.count else {
-                return ""
-            }
-        }
+    // Substring methods
+    func substring(from: Int? = nil, to: Int? = nil) -> String {
+        let start = from ?? 0
+        let end = to ?? self.count - 1
         
-        if let end = to {
-            guard end >= 0 else {
-                return ""
-            }
-        }
+        guard start >= 0, end >= 0, start < self.count, end < self.count, end >= start else { return "" }
         
-        if let start = from, let end = to {
-            guard end - start >= 0 else {
-                return ""
-            }
-        }
+        let startIndex = self.index(self.startIndex, offsetBy: start)
+        let endIndex = self.index(self.startIndex, offsetBy: end + 1)
         
-        let startIndex: String.Index
-        if let start = from, start >= 0 {
-            startIndex = self.index(self.startIndex, offsetBy: start)
-        } else {
-            startIndex = self.startIndex
-        }
-        
-        let endIndex: String.Index
-        if let end = to, end >= 0, end < self.count {
-            endIndex = self.index(self.startIndex, offsetBy: end + 1)
-        } else {
-            endIndex = self.endIndex
-        }
-        
-        return String(self[startIndex ..< endIndex])
+        return String(self[startIndex..<endIndex])
     }
+    
     func substring(from: Int) -> String {
         return self.substring(from: from, to: nil)
     }
+    
     func substring(to: Int) -> String {
         return self.substring(from: nil, to: to)
     }
-    func substring(from: Int?, length: Int) -> String {
-        guard length > 0 else {
-            return ""
-        }
+    
+    func substring(from: Int? = nil, length: Int) -> String {
+        guard length > 0 else { return "" }
         
-        let end: Int
-        if let start = from, start > 0 {
-            end = start + length - 1
-        } else {
-            end = length - 1
-        }
+        let start = from ?? 0
+        let end = start + length - 1
         
-        return self.substring(from: from, to: end)
+        return self.substring(from: start, to: end)
     }
+    
     func substring(length: Int, to: Int?) -> String {
-        guard let end = to, end > 0, length > 0 else {
-            return ""
-        }
+        guard let end = to, end > 0, length > 0 else { return "" }
         
-        let start: Int
-        if let end = to, end - length > 0 {
-            start = end - length + 1
-        } else {
-            start = 0
-        }
-        
-        return self.substring(from: start, to: to)
+        let start = max(0, end - length + 1)
+        return self.substring(from: start, to: end)
     }
 }
